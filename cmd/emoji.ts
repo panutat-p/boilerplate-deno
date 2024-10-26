ef672541bff0371989c19cf36ce48fec88abb3f9
panutat.p <pp.panutat@gmail.com>
2024-10-26 19:18:33 +0700

    feat: emojify

--- cmd/emoji.ts
+++ cmd/emoji.ts
@@ -1,3 +1,7 @@
 import * as emoji from 'npm:node-emoji'
 
-console.log(emoji.emojify(`:sauropod: :heart:  npm`))
+console.log('emojify:', emoji.emojify(`:sauropod: :heart:  deno`))
+
+for (let i = 0; i < 10; i++) {
+  console.log('random:', emoji.random().emoji)
+}
