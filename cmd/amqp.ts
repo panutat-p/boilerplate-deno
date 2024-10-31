2fcb572a8274ef621b584e64adcf618b80ce295d
panutat.p <pp.panutat@gmail.com>
2024-10-31 16:58:01 +0700

    refactor: connect

--- cmd/amqp.ts
+++ cmd/amqp.ts
@@ -3,19 +3,19 @@ import * as amqp from 'npm:amqplib'
 const RABBITMQ_DSN = 'amqp://guest:guest@localhost:5672/'
 const RABBITMQ_QUEUE = '001'
 
-async function connectToRabbitMQ(dsn: string) {
+async function connect(dsn: string) {
   try {
     const connection = await amqp.connect(dsn)
     console.info('Connected to RabbitMQ')
     return connection
-  } catch (error) {
-    console.error('Failed to connect to RabbitMQ', error)
-    throw error
+  } catch (e) {
+    console.error('Failed to connect to RabbitMQ, err:', e)
+    throw e
   }
 }
 
 ;(async () => {
-  const conn = await connectToRabbitMQ(RABBITMQ_DSN)
+  const conn = await connect(RABBITMQ_DSN)
   const ch1 = await conn.createChannel()
   ch1.assertQueue(RABBITMQ_QUEUE, { durable: true })
 })()
